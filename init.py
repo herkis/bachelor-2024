473a6deda1ad99cc949f97b9ec922ad46e8ff6e4
NathanLec <lecluse.eleve@ecole-navale.fr>
2021-10-25 13:56:08 +0200

    Update init.py

--- init.py
+++ init.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.run("colcon build --packages-select sensor_interfaces sensor_thermometer sensor_barometer sensor_oxygen",shell=True)
+subprocess.run("colcon build --packages-select sensor_interfaces sensor_thermometer sensor_barometer sensor_oxygen sensor_salinity",shell=True)
