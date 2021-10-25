952db24749bf3ea7ff3c698a31caf4fc91845a33
NathanLec <lecluse.eleve@ecole-navale.fr>
2021-10-25 13:52:39 +0200

    Thermo_bar_oxy_conduct_Drivers_v1
    The 4 drivers of the sensors use in the ROV project with ROS

---
+++ src/sensor_salinity/sensor_salinity/salinity_data_publisher_node.py
@@ -0,0 +1,34 @@
+from rclpy.node import Node
+
+# tsys01 needed in order to utilize the BlueRobotics TSYS01 Python Library which must be installed
+from sensor_salinity import catlas01
+from sensor_interfaces.msg import Salinity
+
+
+class SalinityDataPublisher(Node):
+    # Initializer 
+    def __init__(self):
+        super().__init__('SalinityDataPublisher')
+        self.publisher_ = self.create_publisher(Salinity, 'salinity_data', 10)
+        read_period = 0.01  # seconds
+        self.timer = self.create_timer(read_period, self.salinity_read_and_publish)
+
+        self.sensor = catlas01.CATLAS01()
+        # if not self.sensor.init():
+        #     print("Sensor could not be initialized")
+        #     exit(1)
+
+    def salinity_read_and_publish(self):
+        # Custom conductivity message to publish. Can be found in the brov2_interfaces.
+        msg = Salinity()
+
+        # Reading thermometer and loading data into custom message
+        if self.sensor.read():
+                msg.salinity_concentration     = self.sensor._salinity
+        else:
+                print("Sensor read failed!")
+                exit(1)
+
+        # Publishing message and logging data sent over the topic /thermometer_data
+        self.publisher_.publish(msg)
+        self.get_logger().info('O: %0.2f mg/L' % (msg.salinity_value))
---
+++ src/sensor_salinity/sensor_salinity/salinity_data_publisher_node_main.py
@@ -0,0 +1,20 @@
+import rclpy
+from sensor_salinity import salinity_data_publisher_node as node
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    
+    # Construct the publisher
+    salinity_data_publisher = node.SalinityDataPublisher()
+
+    # Reading and publishing data at defined rate (0.1 seconds)
+    rclpy.spin(salinity_data_publisher)
+
+    # Clean up when script is stopped
+    salinity_data_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
