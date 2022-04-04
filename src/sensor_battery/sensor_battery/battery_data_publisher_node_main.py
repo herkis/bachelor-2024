ef34b77a78690de53f48b91d8e4c7bcb5c031805
JoJoHTM <100352903+JoJoHTM@users.noreply.github.com>
2022-04-04 13:31:20 +0200

    Added battery to code
    Added folders and files for the battery code the same way as it has been done for  the other sensors

---
+++ src/sensor_battery/sensor_battery/battery_data_publisher_node_main.py
@@ -0,0 +1,1 @@
+#Main battery code
---
+++ src/sensor_battery/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'sensor_battery'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nathan',
+    maintainer_email='jonasgjo@stud.ntnu.no',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'battery_publisher = sensor_battery.battery_data_publisher_node_main:main',
+        ],
+    },
+)
