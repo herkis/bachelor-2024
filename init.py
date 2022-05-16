5addbe16cbc1f3666da46f09c2a140077eb1eb51
JoJoHTM <jojohansenhtm@gmail.com>
2022-05-16 15:46:57 +0200

    Details for further work
    Added more details to what and how to continue with the battery part of the code

--- init.py
+++ init.py
@@ -6,4 +6,6 @@ import subprocess
 # colcon is use to build all the packets after. The packet sensor_interface is use to make an bridge between the messages of the probe and ROS
 # The other packages use sensor_interface to have access to the value and publish them
 
+
+## TODO: Add the battery code the same way the other sensor codes have. i.e. just include sensor_battery here 
 subprocess.run("colcon build --packages-select sensor_interfaces sensor_thermometer sensor_barometer sensor_oxygen sensor_salinity",shell=True)
