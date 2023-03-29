2e64e20697975cdfedb8a3f8e837e12bee4a7d55
Peder <pederbsa@stud.ntnu.no>
2023-03-29 16:50:22 +0200

    Updated the subscriber node

--- src/modem_communication/modem_communication/modem_subscriber.py
+++ src/modem_communication/modem_communication/modem_subscriber.py
@@ -2,22 +2,20 @@ import rclpy
 from rclpy.node import Node
 from sensor_interfaces.msg import Thermometer
 
-# This is an example of how the subscriber node can be written. Very similar to the publisher code
-
 class ModemSubscriberNode(Node):
     
     def __init__(self):
-        #defining variables
-        self.temp_data = [0,0]
-
-
         super().__init__('modem_subscriber')
+
+        #defining variables
+        self.temp_data = [0.0,0.0]
+
         self.temp_subscriber = self.create_subscription(Thermometer, 'thermometer_data', self.temp_callback, 10)
         self.temp_subscriber   # Prevent unused variable warning
 
     def temp_callback(self, msg:Thermometer):
-        self.temp_data[0] = Thermometer.temperature_celsius
-        self.temp_data[1] = Thermometer.local_time
+        self.temp_data[0] = msg.temperature_celsius
+        self.temp_data[1] = msg.local_time
         self.get_logger().info('Extracted %0.2f C at time: %s'% (self.temp_data[0], self.temp_data[1]))
         
 
