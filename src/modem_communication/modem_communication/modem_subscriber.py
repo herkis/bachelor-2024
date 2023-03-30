7c94047c844db5bd4fbf4f2bb52a1d22acd69995
Peder <pederbsa@stud.ntnu.no>
2023-03-30 11:52:18 +0200

    Successfully subscribed to all sensor topics

--- src/modem_communication/modem_communication/modem_subscriber.py
+++ src/modem_communication/modem_communication/modem_subscriber.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from sensor_interfaces.msg import Thermometer
+from sensor_interfaces.msg import *
 
 class ModemSubscriberNode(Node):
     
@@ -8,15 +8,65 @@ class ModemSubscriberNode(Node):
         super().__init__('modem_subscriber')
 
         #defining variables
-        self.temp_data = [0.0,0.0]
+        ##  Convert to dict  ##
+        self.barometer_data = [0.0,0.0]
+        self.oxygen_data = [0.0,0.0]
+        self.salinity_data = [0.0,0.0]
+        self.temperature_data = [0.0,0.0]
 
-        self.temp_subscriber = self.create_subscription(Thermometer, 'sensor_interfaces/msg/Thermometer', self.temp_callback, 10)
-        self.temp_subscriber   # Prevent unused variable warning
+        self.barometer_subscription = self.create_subscription(
+            Barometer, 
+            '/barometer/barometer_data', 
+            self.barometer_callback, 
+            10)
 
-    def temp_callback(self, msg:Thermometer):
-        self.temp_data[0] = msg.temperature_celsius
-        self.temp_data[1] = msg.local_time
-        self.get_logger().info('Extracted %0.2f C at time: %s'% (self.temp_data[0], self.temp_data[1]))
+        self.oxygen_subscription = self.create_subscription(
+            Oxygen, 
+            '/oxygen/oxygen_data', 
+            self.oxygen_callback, 
+            10)
+
+        self.salinity_subscription = self.create_subscription(
+            Salinity, 
+            '/salinity/salinity_data', 
+            self.salinity_callback, 
+            10)
+
+        self.temperature_subscription = self.create_subscription(
+            Thermometer, 
+            '/thermometer/thermometer_data', 
+            self.temperature_callback, 
+            10)
+
+        #Prevent unused variable warning
+        self.barometer_subscription   # Prevent unused variable warning
+        self.oxygen_subscription   # Prevent unused variable warning
+        self.salinity_subscription   # Prevent unused variable warning
+        self.temperature_subscription   # Prevent unused variable warning
+
+    def barometer_callback(self, msg:Barometer):
+        self.barometer_data[0] = msg.pressure_mbar
+        self.barometer_data[1] = msg.local_time
+        self.get_logger().info('Extracted %0.2f mbar at time: %s'% (self.barometer_data[0], self.barometer_data[1]))
+        print('\nhello form logger\n')
+ 
+    def oxygen_callback(self, msg:Oxygen):
+        self.oxygen_data[0] = msg._oxygen_concentration
+        self.oxygen_data[1] = msg.local_time
+        self.get_logger().info('Extracted %0.2f O at time: %s'% (self.oxygen_data[0], self.oxygen_data[1]))
+        print('\nhello form logger\n')
+ 
+    def salinity_callback(self, msg:Salinity):
+        self.salinity_data[0] = msg.salinity_value
+        self.salinity_data[1] = msg.local_time
+        self.get_logger().info('Extracted %0.2f O at time: %s'% (self.salinity_data[0], self.salinity_data[1]))
+        print('\nhello form logger\n')
+ 
+    def temperature_callback(self, msg:Thermometer):
+        self.temperature_data[0] = msg.temperature_celsius
+        self.temperature_data[1] = msg.local_time
+        self.get_logger().info('Extracted %0.2f C at time: %s'% (self.temperature_data[0], self.temperature_data[1]))
+        print('\nhello form logger\n')
         
 
 def main(args=None):
