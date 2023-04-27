8225e9853bdbb0556fef5191975ca488b0687ad1
Peder <pederbsa@stud.ntnu.no>
2023-04-27 20:33:42 +0200

    added logger and changed topic structure

--- launch/sensor.launch.py
+++ launch/sensor.launch.py
@@ -5,54 +5,70 @@ from launch_ros.actions import Node
 # Declare Sample time as float
 sample_time = 10.0
 n_sensors = 5
-#rigg_ID = 1
+transfer_delay = 6.0
 
 
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package='sensor_oxygen',
-            namespace='oxygen',
+            namespace='sensors',
             executable='oxygen_publisher',
-            name='sensors',
+            name='oxygen',
             parameters=[{'sample_time': sample_time}]
         ),
         Node(
             package='sensor_thermometer',
-            namespace='thermometer',
+            namespace='sensors',
             executable='thermometer_publisher',
-            name='sensors',
+            name='thermometer',
             parameters=[{'sample_time': sample_time}]
         ),
         Node(
             package='sensor_barometer',
-            namespace='barometer',
+            namespace='sensors',
             executable='barometer_publisher',
-            name='sensors',
+            name='barometer',
             parameters=[{'sample_time': sample_time}]
         ),
         Node(
             package='sensor_salinity',
-            namespace='salinity',
+            namespace='sensors',
             executable='salinity_publisher',
-            name='sensors',
+            name='salinity',
             parameters=[{'sample_time': sample_time}]
         ),
         Node(
             package='sensor_battery',
-            namespace='battery',
+            namespace='sensors',
             executable='battery_publisher',
-            name='sensors',
+            name='battery',
             parameters=[{'sample_time': sample_time}]
         ),
         Node(
+            package='logger',
+            namespace='logger',
+            executable='internal_logger',
+            name='internal',
+            parameters=[]
+        ),
+        Node(
+            package='logger',
+            namespace='logger',
+            executable='external_logger',
+            name='external',
+            parameters=[]
+        ),
+        Node(
             package='modem_communication',
-            namespace='subnero',
+            namespace='modem',
             executable='modem_subscriber',
-            name='modem',
-            # parameters=[{'rigg_ID': rigg_ID}, # handeled by modem
-            parameters=[{'rigg_ID': rigg_ID},
-                        {'sensor_count': n_sensors}
+            name='subnero',
+            parameters=[
+                        {'sample_time': sample_time},
+                        {'sensor_count': n_sensors},
+                        {'transfer_delay': transfer_delay}
             ]
         ),
     ])
+
